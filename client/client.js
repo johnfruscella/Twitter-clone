36592ff47ca5472132686c9b3d8aa7de941c96f1
johnfruscella <johnfruscella@gmail.com>
2020-01-17 23:58:57 -0500

    added ability to submit data and loading icon

--- client/client.js
+++ client/client.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display= "none";
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get(`name`);
+    const content = formData.get(`content`);
+
+    const mew = {
+        name,
+        content
+    };
+    console.log(mew);
+
+});
+
